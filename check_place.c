3f3a7939d965336c7e0a9935aecb7d4daa875ffa
Mickael DOS SANTOS <mdos-san@e2r6p9.42.fr>
2015-12-10 18:18:25 +0100

    normalize check_place.c

--- check_place.c
+++ check_place.c
@@ -6,56 +6,61 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 13:53:50 by mdos-san          #+#    #+#             */
-/*   Updated: 2015/12/08 10:20:11 by mdos-san         ###   ########.fr       */
+/*   Updated: 2015/12/10 18:15:40 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "dbl_tab.h"
 
-int	check_place(char **tab, char shape[5][5], t_pnt o, int size)
+static int	fcheck(t_pnt *check, t_pnt *first, t_pnt *i, t_pnt *o)
 {
-	t_pnt	first;
-	t_pnt	i;
-	int		cond;
-	int		check_x;
-	int		check_y;
+	check->x = o->x - first->x + i->x;
+	check->y = o->y - first->y + i->y;
+	return (1);
+}
 
-	i.y = 0;
+static void	assign_first(t_pnt *check, t_pnt *first, t_pnt *i, t_pnt *o)
+{
+	first->x = i->x;
+	first->y = i->y;
+	fcheck(check, first, i, o);
+}
+
+static int	return_fnt(char **tab, t_pnt check, int size)
+{
+	if (check.x < 0 || check.y < 0)
+		return (0);
+	if (check.x >= size || check.y >= size)
+		return (0);
+	if (tab[check.y][check.x] != '.')
+		return (0);
+	return (1);
+}
+
+int			check_place(char **tab, char shape[5][5], t_pnt o, int size)
+{
+	t_pnt	i;
+	t_pnt	first;
+	t_pnt	check;
+	int		cond;
+
+	i.y = -1;
 	cond = 0;
-	while (i.y < 4)
+	while (++i.y < 4)
 	{
-		i.x = 0;
-		while (i.x < 4)
-		{
+		i.x = -1;
+		while (++i.x < 4)
 			if (cond == 0 && shape[i.y][i.x] != '.')
 			{
-				first.x = i.x;
-				first.y = i.y;
-				cond = 1;
-				check_x = o.x - first.x + i.x;
-				check_y = o.y - first.y + i.y;
-				if (check_x < 0 || check_y < 0)
-					return (-1);
-				if (check_x >= size || check_y >= size)
-					return (-1);
-				if (tab[check_y][check_x] != '.')
+				cond++;
+				assign_first(&check, &first, &i, &o);
+				if (!return_fnt(tab, check, size))
 					return (0);
 			}
-			else if (shape[i.y][i.x] != '.')
-			{
-				check_x = o.x - first.x + i.x;
-				check_y = o.y - first.y + i.y;
-				if (check_x < 0 || check_y < 0)
-					return (-1);
-				if (check_x >= size || check_y >= size)
-					return (-1);
-				if (tab[check_y][check_x] != '.')
+			else if (shape[i.y][i.x] != '.' && fcheck(&check, &first, &i, &o))
+				if (!return_fnt(tab, check, size))
 					return (0);
-			}
-			i.x = i.x + 1;
-		}
-		i.y = i.y + 1;
 	}
 	return (1);
 }
